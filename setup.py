bcdafa1f22c00e06c261e0aa08269b9daf342c4d
Steffen Hirte <shirte@users.noreply.github.com>
2024-09-25 15:39:26 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ rdkit_requirement = ["rdkit>=2022.3.3"] if not rdkit_installed else []
 
 setup(
     name="nerdd-module",
-    version="0.2.6",
+    version="0.3.0",
     maintainer="Steffen Hirte",
     maintainer_email="steffen.hirte@univie.ac.at",
     packages=find_packages(),
@@ -40,7 +40,6 @@ setup(
     extras_require={
         "dev": [
             "black",
-            "isort",
         ],
         "csp": [
             # note: version 1.0.0 of chembl_structure_pipeline is not available on pypi
