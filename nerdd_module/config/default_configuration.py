161f85c478188432406cbe7bf5c82a9d279d4411
Steffen Hirte <shirte@users.noreply.github.com>
2024-09-29 18:40:00 +0200

    Make black happy

--- nerdd_module/config/default_configuration.py
+++ nerdd_module/config/default_configuration.py
@@ -1,4 +1,4 @@
-from stringcase import snakecase # type: ignore
+from stringcase import snakecase  # type: ignore
 
 from ..polyfills import version
 from .dict_configuration import DictConfiguration
