8d4047dea564c3c94f9b2979cebde6b502f77964
Steffen Hirte <shirte@users.noreply.github.com>
2024-10-03 23:24:12 +0200

    Add types to polyfills submodule

--- nerdd_module/polyfills/get_entry_points.py
+++ nerdd_module/polyfills/get_entry_points.py
@@ -1,3 +1,12 @@
+from typing import Iterable
+
+from typing_extensions import Protocol
+
+
+class EntryPoint(Protocol):
+    def load(self) -> None: ...
+
+
 __all__ = ["get_entry_points"]
 
 # import entry_points from importlib.metadata or fall back to pkg_resources
@@ -5,14 +14,16 @@ __all__ = ["get_entry_points"]
 try:
     from importlib.metadata import entry_points
 
-    def get_entry_points(group):
-        try:
-            return entry_points(group=group)
-        except TypeError:
-            return entry_points().get(group, [])
+    def get_entry_points(group: str) -> Iterable[EntryPoint]:
+        return entry_points(group=group)
+        # TODO: check when this happens:
+        # try:
+        #     return entry_points(group=group)
+        # except TypeError:
+        #     return entry_points().get(group, [])
 
 except ImportError:
     import pkg_resources
 
-    def get_entry_points(group):
+    def get_entry_points(group: str) -> Iterable[EntryPoint]:
         return pkg_resources.iter_entry_points(group)
