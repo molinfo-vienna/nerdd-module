f8b8463ece0f28c0ee842088d5ba52619ee86db2
Steffen Hirte <shirte@users.noreply.github.com>
2024-10-05 15:28:05 +0200

    Fix config classes for mypy

--- nerdd_module/config/yaml_configuration.py
+++ nerdd_module/config/yaml_configuration.py
@@ -1,13 +1,13 @@
 import base64
 import os
-from ..polyfills import PathLikeStr
 from pathlib import Path
-from typing import Any, Optional, Union
+from typing import IO, Any, Optional, Union
 
 import filetype  # type: ignore
 import yaml
 from typing_extensions import Protocol
 
+from ..polyfills import PathLikeStr
 from .configuration import Configuration
 
 __all__ = ["YamlConfiguration"]
@@ -39,19 +39,29 @@ def image_constructor(loader: CustomLoaderLike, node: yaml.Node) -> str:
 class YamlConfiguration(Configuration):
     def __init__(
         self,
-        handle: Union[str, PathLikeStr],
+        path_or_handle: Union[str, PathLikeStr, IO[str]],
         base_path: Union[str, PathLikeStr, None] = None,
     ) -> None:
         super().__init__()
 
-        if isinstance(handle, str):
-            handle = Path(handle)
+        if isinstance(path_or_handle, str):
+            path_or_handle = Path(path_or_handle)
 
         if base_path is None:
-            assert os.path.isfile(handle), f"File {handle} does not exist"
-            base_path = os.path.dirname(handle)
+            assert isinstance(path_or_handle, Path) and os.path.isfile(
+                path_or_handle
+            ), f"File {path_or_handle} does not exist"
+            base_path = os.path.dirname(path_or_handle)
         else:
             base_path = Path(base_path)
+
+        handle: IO[str]
+        if isinstance(path_or_handle, Path):
+            handle = path_or_handle.open()
+        elif hasattr(path_or_handle, "__fspath__"):
+            handle = open(path_or_handle)
+        else:
+            handle = path_or_handle
 
         # we want to parse and process special tags (e.g. !image) in yaml files
         # when loading a file with !image, the specified path should be relative to
@@ -65,7 +75,7 @@ class YamlConfiguration(Configuration):
 
         yaml.add_constructor("!image", image_constructor, CustomLoader)
 
-        self.yaml = yaml.load(open(handle, "r"), Loader=CustomLoader)
+        self.yaml = yaml.load(handle, Loader=CustomLoader)
 
     def _get_dict(self) -> dict:
         return self.yaml["module"]
