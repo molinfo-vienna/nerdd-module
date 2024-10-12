950c723deadb4fcb5feaf9d8a9191f9f47122632
Steffen Hirte <shirte@users.noreply.github.com>
2024-10-12 13:31:09 +0200

    Use importlib.abc.Traversable for old Python versions

--- nerdd_module/polyfills/files.py
+++ nerdd_module/polyfills/files.py
@@ -6,5 +6,8 @@ if sys.version_info < (3, 9):
     from importlib_resources import as_file, files
     from importlib_resources.abc import Traversable
 else:
+    try:
+        from importlib.abc import Traversable
+    except ImportError:
+        from importlib.resources.abc import Traversable
     from importlib.resources import as_file, files
-    from importlib.resources.abc import Traversable
