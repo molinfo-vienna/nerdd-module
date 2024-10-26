0812a4a7a2d20292cccc5668633566634669ecb0
Steffen Hirte <shirte@users.noreply.github.com>
2024-10-26 22:50:04 +0200

    fix: add missing export to __init__

--- nerdd_module/__init__.py
+++ nerdd_module/__init__.py
@@ -3,6 +3,7 @@ from .model import *
 from .polyfills import get_entry_points
 from .problem import *
 from .version import *
+from .output import *
 
 for entry_point in get_entry_points("nerdd-module.plugins"):
     entry_point.load()
