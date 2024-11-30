a701ac6772e03b8526be854605e0afe09eb00485
Steffen Hirte <shirte@users.noreply.github.com>
2024-11-30 03:11:56 +0100

    Implement and test problem list converter

--- tests/basic/converters/test_converters.py
+++ tests/basic/converters/test_converters.py
@@ -1,5 +1,11 @@
+from nerdd_module import Problem
 from nerdd_module.config import ResultProperty
-from nerdd_module.converters import Converter, IdentityConverter, VoidConverter
+from nerdd_module.converters import (
+    Converter,
+    IdentityConverter,
+    ProblemListConverter,
+    VoidConverter,
+)
 
 primitive_data_types = [
     "int",
@@ -25,3 +31,19 @@ def test_non_existing_data_type():
     for output_format in output_formats:
         converter = Converter.get_converter(result_property, output_format)
         assert isinstance(converter, VoidConverter)
+
+
+def test_problem_list_converter():
+    result_property = ResultProperty(name="test", type="problem_list")
+    problem_list = [Problem("problem_type", "problem_description")]
+    for output_format in output_formats:
+        converter = Converter.get_converter(result_property, output_format)
+        converted_value = converter.convert(problem_list, {})
+        assert isinstance(converter, ProblemListConverter)
+        if output_format in ["pandas", "record_list", "iterator"]:
+            assert isinstance(converted_value, list)
+            assert len(converted_value) == len(problem_list)
+            assert isinstance(converted_value[0], Problem)
+        else:
+            assert isinstance(converted_value, str)
+            assert converted_value.startswith("problem_type")
