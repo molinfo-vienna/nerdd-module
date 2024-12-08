72451d5f9f4aba69af24cdd6f6ab01bf97864aaf
Steffen Hirte <shirte@users.noreply.github.com>
2024-12-09 00:35:23 +0100

    Add RepresentationConverter

--- nerdd_module/converters/__init__.py
+++ nerdd_module/converters/__init__.py
@@ -4,4 +4,5 @@ from .identity_converter import *
 from .mol_converter import *
 from .mol_to_image_converter import *
 from .problem_list_converter import *
+from .representation_converter import *
 from .void_converter import *
