28ba0c407f27fe14c3c67fd46ac0a2f90856ceec
Steffen Hirte <shirte@users.noreply.github.com>
2025-01-06 17:49:18 +1300

    fix: Allow strings in problem lists

--- nerdd_module/converters/problem_list_converter.py
+++ nerdd_module/converters/problem_list_converter.py
@@ -1,4 +1,5 @@
-from typing import Any, List, cast
+import logging
+from typing import Any, List, Union, cast
 
 from ..problem import Problem
 from .converter import Converter
@@ -6,14 +7,24 @@ from .converter_config import ALL, ConverterConfig
 
 __all__ = ["ProblemListConverter"]
 
+logger = logging.getLogger(__name__)
+
 
 class ProblemListConverter(Converter):
     def _convert(self, input: Any, context: dict) -> Any:
         if self.output_format in ["pandas", "iterator", "record_list"]:
             return input
         else:
-            problem_list: List[Problem] = cast(List[Problem], input)
-            return "; ".join([f"{problem.type}: {problem.message}" for problem in problem_list])
+            problem_list: List[Union[Problem, str]] = cast(List[Union[Problem, str]], input)
+
+            def _represent(problem: Union[Problem, str]) -> str:
+                if isinstance(problem, Problem):
+                    return f"{problem.type}: {problem.message}"
+                else:
+                    logger.warning("Item is not an instance of Problem: %s", problem)
+                    return problem
+
+            return "; ".join([_represent(problem) for problem in problem_list])
 
     config = ConverterConfig(
         data_types="problem_list",
