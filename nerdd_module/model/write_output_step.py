2cebca608fa5eb048d8e5605f20f2839cd12ae2d
Steffen Hirte <shirte@users.noreply.github.com>
2025-03-09 03:08:31 +1300

    fix: Require config parameter in WriteOutputStep

--- nerdd_module/model/write_output_step.py
+++ nerdd_module/model/write_output_step.py
@@ -1,5 +1,6 @@
 from typing import Any, Iterator
 
+from ..config import Module
 from ..output import Writer
 from ..steps import OutputStep
 
@@ -7,16 +8,17 @@ __all__ = ["WriteOutputStep"]
 
 
 class WriteOutputStep(OutputStep):
-    def __init__(self, output_format: str, **kwargs: Any) -> None:
+    def __init__(self, output_format: str, config: Module, **kwargs: Any) -> None:
         super().__init__()
         self._output_format = output_format
-        self._kawrgs = kwargs
+        self._config = config
+        self._kwargs = kwargs
 
     def _get_result(self, source: Iterator[dict]) -> Any:
         # get the correct output writer
-        writer = Writer.get_writer(self._output_format, **self._kawrgs)
+        writer = Writer.get_writer(self._output_format, config=self._config, **self._kwargs)
         result = writer.write(source)
         return result
 
     def __repr__(self) -> str:
-        return f"WriteOutputStep(output_format={self._output_format}, kwargs={self._kawrgs})"
+        return f"WriteOutputStep(output_format={self._output_format}, kwargs={self._kwargs})"
