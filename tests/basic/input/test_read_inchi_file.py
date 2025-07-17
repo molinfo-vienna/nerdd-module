fc9b4db6af0518e91d63f624784dad3577c3b4e7
Steffen Hirte <shirte@users.noreply.github.com>
2025-07-17 14:09:30 +1200

    test: Add test for reading inchi files

---
+++ tests/basic/input/test_read_inchi_file.py
@@ -0,0 +1,18 @@
+import os
+
+from nerdd_module.input import DepthFirstExplorer
+
+HERE = os.path.dirname(__file__)
+
+
+def test_read_inchi_file():
+    explorer = DepthFirstExplorer()
+
+    results = list(explorer.explore(os.path.join(HERE, "data", "test.inchi")))
+
+    # there are 5 lines in the test.inchi file
+    assert len(results) == 5
+
+    for i, r in enumerate(results):
+        # only the first 3 rows are valid
+        assert len(r.errors) == (0 if i not in [3,4] else 1)
