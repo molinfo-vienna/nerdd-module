63cc08a53b5b52c89f17189d697b98621e1eebd8
Steffen Hirte <shirte@users.noreply.github.com>
2025-07-20 14:25:56 +1200

    test: Check if byte strings are accepted

---
+++ tests/basic/input/test_read_inchi_string.py
@@ -0,0 +1,41 @@
+from rdkit.Chem import Mol
+
+from nerdd_module.input import DepthFirstExplorer
+
+
+def test_read_inchi_string():
+    explorer = DepthFirstExplorer()
+
+    inp = (
+        "InChI=1S/C14H20N2O4S/c1-2-20-14(17)15-8-10-16(11-9-15)21(18,19)12-13-6-4-3-5-7-13"
+        "/h3-7H,2,8-12H2,1H3"
+    )
+
+    results = list(explorer.explore(inp))
+
+    assert len(results) == 1
+
+    result = results[0]
+
+    assert result.raw_input == inp
+    assert result.input_type == "inchi"
+    assert isinstance(result.mol, Mol)
+
+
+def test_read_inchi_bytes():
+    explorer = DepthFirstExplorer()
+
+    inp = (
+        b"InChI=1S/C14H20N2O4S/c1-2-20-14(17)15-8-10-16(11-9-15)21(18,19)12-13-6-4-3-5-7-13"
+        b"/h3-7H,2,8-12H2,1H3"
+    )
+
+    results = list(explorer.explore(inp))
+
+    assert len(results) == 1
+
+    result = results[0]
+
+    assert result.raw_input == inp.decode("utf-8")
+    assert result.input_type == "inchi"
+    assert isinstance(result.mol, Mol)
--- tests/basic/input/test_read_smiles_string.py
+++ tests/basic/input/test_read_smiles_string.py
@@ -8,11 +8,24 @@ def test_read_smiles_string():
 
     results = list(explorer.explore("CCCCCCCCCCCCO"))
 
-    # there are 13 lines in the test.smiles file
     assert len(results) == 1
 
     result = results[0]
-    
+
+    assert result.raw_input == "CCCCCCCCCCCCO"
+    assert result.input_type == "smiles"
+    assert isinstance(result.mol, Mol)
+
+
+def test_read_smiles_bytes():
+    explorer = DepthFirstExplorer()
+
+    results = list(explorer.explore(b"CCCCCCCCCCCCO"))
+
+    assert len(results) == 1
+
+    result = results[0]
+
     assert result.raw_input == "CCCCCCCCCCCCO"
     assert result.input_type == "smiles"
     assert isinstance(result.mol, Mol)
