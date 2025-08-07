6e6923f2ee2a6e19595f2fce70d098675422458b
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-07 13:17:37 +1200

    test: Check source when reading smiles file

--- tests/basic/input/test_read_smiles_file.py
+++ tests/basic/input/test_read_smiles_file.py
@@ -8,11 +8,15 @@ HERE = os.path.dirname(__file__)
 def test_read_smiles_file():
     explorer = DepthFirstExplorer()
 
-    results = list(explorer.explore(os.path.join(HERE, "data", "test.smiles")))
+    input_file = os.path.join(HERE, "data", "test.smiles")
+
+    results = list(explorer.explore(input_file))
 
     # there are 13 lines in the test.smiles file
     assert len(results) == 13
 
     for i, r in enumerate(results):
+        assert len(r.source) == 1
+        assert r.source[0] == input_file
         # all results except the last one are valid
         assert len(r.errors) == (0 if i != 12 else 1)
