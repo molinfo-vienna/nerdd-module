7636c413d7610207f1eb8cc184f56ce75e03e5b0
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-12 00:02:28 +1200

    fix: Handle exceptions in TarReader

--- nerdd_module/input/tar_reader.py
+++ nerdd_module/input/tar_reader.py
@@ -1,6 +1,7 @@
 import tarfile
 from typing import Any, Iterator, Tuple
 
+from ..problem import Problem
 from .reader import ExploreCallable, MoleculeEntry, Reader
 
 __all__ = ["TarReader"]
@@ -20,15 +21,30 @@ class TarReader(Reader):
             for member in tar.getmembers():
                 if not member.isfile():
                     continue
-                for entry in explore(tar.extractfile(member)):
-                    # the underlying reader only sees the file content as a stream
-                    # -> it might believe that the source is "raw_input"
-                    # -> we need to correct that here
-                    if len(entry.source) == 1 and entry.source[0] == "raw_input":
-                        source: Tuple[str, ...] = tuple()
-                    else:
-                        source = entry.source
-                    yield entry._replace(source=(member.name, *source))
+
+                try:
+                    for entry in explore(tar.extractfile(member)):
+                        # the underlying reader only sees the file content as a stream
+                        # -> it might believe that the source is "raw_input"
+                        # -> we need to correct that here
+                        if len(entry.source) == 1 and entry.source[0] == "raw_input":
+                            source: Tuple[str, ...] = tuple()
+                        else:
+                            source = entry.source
+                        yield entry._replace(source=(member.name, *source))
+                except Exception as e:
+                    yield MoleculeEntry(
+                        raw_input="<tar>",
+                        input_type="unknown",
+                        source=(member.name,),
+                        mol=None,
+                        errors=[
+                            Problem(
+                                "invalid_tar_member",
+                                f"Could not read tar member '{member.name}': {e}",
+                            )
+                        ],
+                    )
 
     def __repr__(self) -> str:
         return "TarReader()"
