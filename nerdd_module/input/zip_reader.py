d3a897c8cf00b1aacd0c792320c900cf4761413b
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-11 23:55:47 +1200

    fix: Handle exceptions in ZipReader

--- nerdd_module/input/zip_reader.py
+++ nerdd_module/input/zip_reader.py
@@ -1,6 +1,7 @@
 import zipfile
 from typing import Any, Iterator, Tuple
 
+from ..problem import Problem
 from .reader import ExploreCallable, MoleculeEntry, Reader
 
 __all__ = ["ZipReader"]
@@ -21,17 +22,31 @@ class ZipReader(Reader):
                 # check if the member is a file
                 if member.endswith("/"):
                     continue
-                with zipf.open(member, "r") as f:
-                    for entry in explore(f):
-                        # the underlying reader only sees the file content as a stream
-                        # -> it might believe that the source is "raw_input"
-                        # -> we need to correct that here
-                        if len(entry.source) == 1 and entry.source[0] == "raw_input":
-                            source: Tuple[str, ...] = tuple()
-                        else:
-                            source = entry.source
 
-                        yield entry._replace(source=(member, *source))
+                try:
+                    with zipf.open(member, "r") as f:
+                        for entry in explore(f):
+                            # the underlying reader only sees the file content as a stream
+                            # -> it might believe that the source is "raw_input"
+                            # -> we need to correct that here
+                            if len(entry.source) == 1 and entry.source[0] == "raw_input":
+                                source: Tuple[str, ...] = tuple()
+                            else:
+                                source = entry.source
+
+                            yield entry._replace(source=(member, *source))
+                except Exception as e:
+                    yield MoleculeEntry(
+                        raw_input="<zip>",
+                        input_type="unknown",
+                        source=(member,),
+                        mol=None,
+                        errors=[
+                            Problem(
+                                "invalid_zip_member", f"Could not read zip member '{member}': {e}"
+                            )
+                        ],
+                    )
 
     def __repr__(self) -> str:
         return "ZipReader()"
