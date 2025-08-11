1f68699f13e93ec1945198897d11d51df524b179
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-12 00:00:24 +1200

    fix: Handle exceptions in GzipReader

--- nerdd_module/input/gzip_reader.py
+++ nerdd_module/input/gzip_reader.py
@@ -1,6 +1,7 @@
 import gzip
 from typing import Any, Iterator
 
+from ..problem import Problem
 from .reader import ExploreCallable, MoleculeEntry, Reader
 
 __all__ = ["GzipReader"]
@@ -22,7 +23,16 @@ class GzipReader(Reader):
             f.read(1)
             f.seek(0)
 
-            yield from explore(f)
+            try:
+                yield from explore(f)
+            except Exception as e:
+                yield MoleculeEntry(
+                    raw_input="<gzip>",
+                    input_type="gzip",
+                    source=("raw_input",),
+                    mol=None,
+                    errors=[Problem("invalid_input", f"Invalid gzip file: {e}")],
+                )
 
     def __repr__(self) -> str:
         return "GzipReader()"
