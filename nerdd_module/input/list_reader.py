4a9ffc29024242ebd81ea00f2c1a4bd789ec4182
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-13 00:03:21 +1200

    fix: Handle exceptions in ListReader

--- nerdd_module/input/list_reader.py
+++ nerdd_module/input/list_reader.py
@@ -1,6 +1,7 @@
 from io import IOBase
 from typing import Any, Iterable, Iterator
 
+from ..problem import Problem
 from .reader import ExploreCallable, MoleculeEntry, Reader
 
 __all__ = ["ListReader"]
@@ -16,7 +17,19 @@ class ListReader(Reader):
         ), f"input must be an iterable, but is {type(input_iterable)}"
 
         for entry in input_iterable:
-            yield from explore(entry)
+            try:
+                yield from explore(entry)
+            except Exception as e:
+                raw_input = str(entry)
+                if len(raw_input) > 100:
+                    raw_input = raw_input[:97] + "..."
+                yield MoleculeEntry(
+                    raw_input=raw_input,
+                    input_type="unknown",
+                    source=(),
+                    mol=None,
+                    errors=[Problem("invalid_list_entry", f"Could not read list entry: {e}")],
+                )
 
     def __repr__(self) -> str:
         return "ListReader()"
